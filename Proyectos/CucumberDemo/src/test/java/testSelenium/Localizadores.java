2955f7e69b0ff3badddf24316fb26f6edf9ca3ad
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-04-26 05:44:56 -0300

    Update Localizadores.java

--- Proyectos/CucumberDemo/src/test/java/testSelenium/Localizadores.java
+++ Proyectos/CucumberDemo/src/test/java/testSelenium/Localizadores.java
@@ -1,5 +1,18 @@
 package testSelenium;
 
-public class Localizadores {
+import org.junit.Test;
+import org.openqa.selenium.By;
 
+public class Localizadores {
+@Test
+	void localizadores() {
+		By locator=By.id("id_del_elemento");
+		By locator_name = By.name("name_element");
+		By locator_className = By.className("clase_elemento");
+		By locator_tagName = By.tagName("tag");
+		By locator_linkText = By.linkText("texto_link");
+		By locator_partialLinkText = By.partialLinkText("textoPartial_link");
+		By locator_cssSelector=By.cssSelector("input[name='q']");
+		By locator_Xpath = By.xpath("//input[@name='q']");
+	}
 }
