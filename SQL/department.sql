20e0a466cf5f4b214a2958cd994c907056f4412e
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-04-27 05:16:31 -0300

    Add files via upload

--- SQL/department.sql
+++ SQL/department.sql
@@ -3,11 +3,10 @@ CREATE TABLE capgemini_asturias.department (
   location_id INT NOT NULL,
   PRIMARY KEY (department_id, location_id));
 
-INSERT INTO indragt3.department (department_id, location_id) VALUES ('90', '1400');
-INSERT INTO indragt3.department (department_id, location_id) VALUES ('60', '1500');
-INSERT INTO indragt3.department (department_id, location_id) VALUES ('50', '1700');
-INSERT INTO indragt3.department (department_id, location_id) VALUES ('80', '1800');
-INSERT INTO indragt3.department (department_id, location_id) VALUES ('10', '2500');
-INSERT INTO indragt3.department (department_id, location_id) VALUES ('20', '1400');
-INSERT INTO indragt3.department (department_id, location_id) VALUES ('110', '1700');
-
+INSERT INTO capgemini_asturias.department (department_id, location_id) VALUES ('90', '1400');
+INSERT INTO capgemini_asturias.department (department_id, location_id) VALUES ('60', '1500');
+INSERT INTO capgemini_asturias.department (department_id, location_id) VALUES ('50', '1700');
+INSERT INTO capgemini_asturias.department (department_id, location_id) VALUES ('80', '1800');
+INSERT INTO capgemini_asturias.department (department_id, location_id) VALUES ('10', '2500');
+INSERT INTO capgemini_asturias.department (department_id, location_id) VALUES ('20', '1400');
+INSERT INTO capgemini_asturias.department (department_id, location_id) VALUES ('110', '1700');
