cd619af9266552c89b4d3654c53f801d965610b1
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-05-12 06:50:09 -0300

    Add files via upload

---
+++ Proyectos/thymeleaf-google-login/src/main/java/com/security/thymeleafgooglelogin/ThymeleafGoogleLoginApplication.java
@@ -0,0 +1,13 @@
+package com.security.thymeleafgooglelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafGoogleLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafGoogleLoginApplication.class, args);
+	}
+
+}
